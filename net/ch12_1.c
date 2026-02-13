05f07d544a6280afe69a47cc03b33803a93465fe
epqlffltm <kjh55984249@gmail.com>
2026-02-13 17:48:37 +0900

    호스트명 읽어오기

---
+++ net/ch12_1.c
@@ -0,0 +1,22 @@
+/*
+2026-02-13
+호스트명 읽어오기
+*/
+
+#include<stdio.h>
+#include<netdb.h>
+
+int main(void)
+{
+  struct hostnet *hent;
+
+  sethostent(0);
+
+  while((*hent = gethostent()) != NULL)
+  {
+    printf("hostname: %s\n", hent->h_name);
+  }
+  endhostent();
+
+  return 0;
+}
