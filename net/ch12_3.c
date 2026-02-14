d2deff8f099512398c3a113debb305ab495da7a0
epqlffltm <kjh55984249@gmail.com>
2026-02-14 09:52:09 +0900

    NBO(네트워크 바이트 순서) -> BHO(호스트 바이트 순서)로 변환

---
+++ net/ch12_3.c
@@ -0,0 +1,22 @@
+/*
+NBO(네트워크 바이트 순서) -> BHO(호스트 바이트 순서)로 변환
+*/
+
+#include<stdio.h>
+#include<netdb.h>
+
+int main(void)
+{
+  struct servent *port;
+  serservent(0);
+
+  for(int i = 0; i < 5; i++)
+  {
+    port = getservent();
+    printf("name = %s\n port = %d\n", port -> s_name, ntohs(port -> s_port));
+  }
+
+  endservent();
+
+  return 0;
+}
