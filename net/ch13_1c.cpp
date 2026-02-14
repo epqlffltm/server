39f9b715a7443430db12eb1ddaff5213a0b785ff
epqlffltm <kjh55984249@gmail.com>
2026-02-14 17:23:50 +0900

    반복 실행 서버

---
+++ net/ch13_1c.cpp
@@ -0,0 +1,52 @@
+/*
+2026-02-14
+반복 실행 서버(클라이언트)
+*/
+
+#include<iostream>
+#include<cstdlib>
+#include<string.h>
+#include<cstring>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<sys/cocket.h>
+
+int main(void)
+{
+  const std::string port_number = "9001";
+  int sd = -1;
+  char buf[256];
+  struct sockaddr_in sin;
+
+  memset((char *)&sin, '\0',sizeof(sin));
+  sin.sin_family = AF_INET;
+  sin.sin_port = hrons(port_number);
+  sin.sin_addr.s_addr = inet_addr(INADDR_ANY);
+
+  if((sd = socket(AF_INET, SOCK_STREAM,0)) == -1)
+  {
+    std::cerr << "socket error" << std::endl;
+    exit(1);
+  }
+
+  if(connect(sd,(struct sockaddr *)&sin,sizeof(sin)))
+  {
+    std::cerr << "connect" << std::endl;
+    exit(1);
+  }
+
+  std::cout << "from server: " << buf <<std::endl;
+
+  strcpy(buf, "i want a http service");
+  if(send(sd, buf, size(buf)+1,0) == -1)
+  {
+    std::cerr << "send error" << std::end;
+    exit(1);
+  }
+
+  close(sd);
+
+  return 0;
+}
---
+++ net/ch13_1s.cpp
@@ -0,0 +1,73 @@
+/*
+2026-02-14
+반복 실행 서버
+*/
+
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<sys/socket.h>
+
+int main(void)
+{
+  const std::string port_number = "9001";
+  char buf[256];
+  struct sockaddr_in sin, cli;
+  int sd  = -1, ns = -1, clientlen = sizeof(cli);
+
+  memset((char *)&sin, '\0', sizeof(sin));
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons(port_number);
+  sin.sin_addr.s_addr = inet_addr(INADDR_ANY);
+  
+  if((sd = socker(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    std::cerr << "socket error" << std::endl;
+    exit(1);
+  }
+
+  if(bind(sd,(struct sockaddr *)&sin, sizeof(sin)))
+  {
+    std::cerr << "bind" << std::endl;
+    exit(1);
+  }
+
+  if(listen(sd, 5))
+  {
+    std::cerr << "listen error" << std::endl;
+    exit(1);
+  }
+
+  while(true)
+  {
+    if((ns = accept(sd, (struct sockaddr *)&cli, &clientlen)) == -1)
+    {
+      std::cerr << "accept error" << std::endl;
+      exit(1);
+    }
+    sprintf(buf, "%s", inet_ntoa(cli.sin_addr));
+    std::cout << "message : " << buf << std::endl;
+
+    strcpy(buf, "welcome to network server");
+    if(send(ns, buf, strlen(buf) + 1,0) == -1)
+    {
+      std::cerr << "send error" << std::endl;
+      exit(1);
+    }
+
+    if(recv(ns,buf,sizeof(buf),0) == =1)
+    {
+      std::cerr << "recv error" << std::endl;
+      exit(1);
+    }
+    std::cout << "from client : " << buf << std::endl;
+    close(ns);
+  }
+  close(sd);
+
+  return 0;
+}
